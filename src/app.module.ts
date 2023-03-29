9d2e5f976f5633f43d15e44c62093b695a7cbb98
Ali Arain <deistali2@gmail.com>
2023-03-30 03:14:10 +0500

    Common and Users Module Added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, CommonModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
